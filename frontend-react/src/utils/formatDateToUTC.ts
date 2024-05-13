ca8b7aff7290ce62492060b2e89768ecfb492b4c
fgmatheus <125808812+fgmatheus@users.noreply.github.com>
2024-05-13 13:49:16 -0300

    .

--- frontend-react/src/utils/formatDateToUTC.ts
+++
@@ -1,6 +0,0 @@
-export function formatDateToUTC(date: string) {
-  const formattedDate = new Date(date);
-  const offset = formattedDate.getTimezoneOffset(); // Obtém o offset em minutos
-  formattedDate.setMinutes(formattedDate.getMinutes() + offset); // Ajusta para UTC+0
-  return formattedDate.toISOString().slice(0, 19) + "Z"; // Retorna a data no formato UTC+0
-}
